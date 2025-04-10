2919e065f9451bb92b7522feff8279d95f2939f9
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-04-10 10:22:27 +0200

    Kontantstøtte skjules så lenge den ikke er støttet

--- frontend/src/typer/ytelse.ts
+++ frontend/src/typer/ytelse.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export const ytelseSchema = z.enum(['Overgangsstønad', 'Barnetrygd', 'Kontantstøtte'], {
+export const ytelseSchema = z.enum(['Overgangsstønad', 'Barnetrygd' /*, 'Kontantstøtte'*/], {
     message: 'Ytelse er påkrevd',
 });
 export type Ytelse = z.infer<typeof ytelseSchema>;
@@ -12,12 +12,12 @@ export const ytelseGrupper = [
     },
     {
         fagsystem: 'Barnetrygd og kontantstøtte',
-        ytelser: [ytelseSchema.enum.Barnetrygd, ytelseSchema.enum.Kontantstøtte],
+        ytelser: [ytelseSchema.enum.Barnetrygd /*, ytelseSchema.enum.Kontantstøtte*/],
     },
 ] as const;
 
 export const månedsytelser = [
     ytelseSchema.enum.Overgangsstønad,
     ytelseSchema.enum.Barnetrygd,
-    ytelseSchema.enum.Kontantstøtte,
+    /*ytelseSchema.enum.Kontantstøtte,*/
 ];
